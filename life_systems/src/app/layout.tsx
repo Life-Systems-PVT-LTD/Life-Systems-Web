faa923543211ee80771dfaa6772163b8710a1530
sanjana-kumara <sanjanakumara92@gmail.com>
2025-06-05 02:35:22 +0530

    Complete Why Choose Us Page

--- life_systems/src/app/layout.tsx
+++ life_systems/src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import WhyChooseUs from "./components/Why_choose_us";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -34,6 +35,8 @@ export default function RootLayout({
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
         
         {children}
+
+        <WhyChooseUs />
 
       </body>
     </html>
