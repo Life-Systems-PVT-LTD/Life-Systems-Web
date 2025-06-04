f45c82f675d5d6e11113cae33ee173ce8a2b2464
Donato Shenuka Randeniya <124884711+donatoshenukarandeniya@users.noreply.github.com>
2025-06-05 01:49:37 +0530

    intro completed

--- life_systems/src/app/layout.tsx
+++ life_systems/src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Intro from "./components/intro"
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -32,7 +33,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
-        
+        <Intro/>
         {children}
 
       </body>
