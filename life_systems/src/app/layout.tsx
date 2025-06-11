1b6b201703e2f02f2627f75fecf828679a2d1654
Pasindu Nethsara <pnethsara32@gmail.com>
2025-06-12 00:51:46 +0530

    completed and updated

--- life_systems/src/app/layout.tsx
+++ life_systems/src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import Footer from "./components/Footer/Footer";
+import Footer from "../components/Footer/Footer";
 
 import Navbar from "../components/NavBar/navbar";
 
@@ -43,7 +43,12 @@ export default function RootLayout({ children, }: Readonly<{ children: React.Rea
 
         {children}
 
-        <Footer />
+        <div className="mt-16">
+
+          <Footer />
+
+        </div>
+
 
       </body>
 
