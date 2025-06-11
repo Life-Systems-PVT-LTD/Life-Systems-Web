1bb8ad1428d1c682dc3ada6eedcf890c7678fcd1
Pasindu Nethsara <pnethsara32@gmail.com>
2025-06-11 22:42:09 +0530

    2025-06-11-why choose us Complete-Pasindu Nethsara

--- life_systems/src/app/layout.tsx
+++ life_systems/src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import WhyChooseUs from "./components/Why_choose_us";
+import WhyChooseUs from "./components/why_choose_us/Why_choose_us";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
